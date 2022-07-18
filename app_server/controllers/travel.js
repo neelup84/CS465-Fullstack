7ac89a1a9a937a9370abae4af7c74ccc63d73f7d
neelup84 <https://github.com/neelup84>
2022-07-18 00:00:50 -0500

    convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/*GET traveler view*/
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
     res.render('travel', {title: pageTitle});
@@ -5,4 +9,4 @@ const travel = (req, res) => {
 
 module.exports = {
     travel
-}
+} 
