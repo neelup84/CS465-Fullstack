859ea6f6a08dd23a83d88a22020a82617c7c531a
neelup84 <https://github.com/neelup84>
2022-07-24 15:23:19 -0500

    some fixes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
 
 //register handlebar partials (https://www.npmjs.com/package/hbs)
   hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
